6a41502d259815ec2674f8e48b85ee6f73b725df
Gustavo <gustavochiroli@hotmail.com>
2024-06-19 21:00:14 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import NavBar from "./_components/NavBar";
 import FirstSession from "./_sections/firstsection";
 
 
 const Home = () => {
   return (
     <>
+      <NavBar />
       <FirstSession />
     </>
   );
